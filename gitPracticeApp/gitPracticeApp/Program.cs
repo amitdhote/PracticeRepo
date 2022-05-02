837282658bcedaee7496adbf75ca3c2b1fb99ba7
amit <amit.dhote@avl.com>
2022-05-02 10:57:44 +0530

    second commit

--- gitPracticeApp/gitPracticeApp/Program.cs
+++ gitPracticeApp/gitPracticeApp/Program.cs
@@ -7,6 +7,7 @@ namespace gitPracticeApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello! I am Amit");
         }
     }
 }
